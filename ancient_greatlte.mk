257408e10e42c0de02c5491116e0c10532144a18
Mocarafee <harif.rp05@gmail.com>
2020-06-26 08:30:33 +0700

    greatlte: ancient bringup

--- ancient_greatlte.mk
+++ ancient_greatlte.mk
@@ -22,15 +22,17 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 $(call inherit-product, device/samsung/greatlte/device.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Ancient stuff.
+$(call inherit-product, vendor/ancient/config/common_full_phone.mk)
+TARGET_GAPPS_ARCH := arm64
+TARGET_INCLUDE_STOCK_ARCORE := true
 
 TARGET_BOOT_ANIMATION_RES := 1440
-SAKURA_OFFICIAL=true
+
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := greatlte
-PRODUCT_NAME := lineage_greatlte
+PRODUCT_NAME := ancient_greatlte
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-N950F
 PRODUCT_MANUFACTURER := samsung
